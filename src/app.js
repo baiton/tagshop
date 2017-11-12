050ec3724100415fb85939c92556a812ac54cab4
patebry <patebry@gmail.com>
2017-11-12 12:35:20 -0500

    merging

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import history from './history'
 import Home from './pages/home'
-
+import Verify from './pages/verify'
 import UserStore from './pages/UserStore'
 import Cart from './pages/cart'
 
@@ -11,6 +11,7 @@ const App = () => {
 		<BrowserRouter history={history}>
 			<div>
 				<Switch>
+					<Route path="/verify" component={Verify}>
 					<Route path="/cart" component={Cart} />
 					<Route path="/:username" component={UserStore} />
 					<Route exact path="/" component={Home} />
---
+++ src/pages/verify.js
@@ -0,0 +1,47 @@
+const React = require('react')
+const { connect } = require('react-redux')
+
+function Verify(props) {
+	console.log(props)
+	return (
+		<div className="avenir">
+			<div>
+				<img
+					className="flex center"
+					src="http://tagshop.co/assets/media/brand250.png"
+					alt="TagShop"
+				/>
+			</div>
+			<div className="dt-ns dt--fixed-ns">
+				<div className="dtc-ns tc pa4" />
+				<div className="dtc-ns tc h7 pv4 bg-black-05 br3">
+					<a
+						className="no-underline near-white bg-animate bg-blue hover-bg-gray inline-flex items-center ma2 tc br2 pa2"
+						href="https://instagram.com/"
+						title="Instagram"
+					>
+						<img
+							src="http://library.austintexas.gov/sites/default/files/instagram-logo-white.png"
+							className="dib h2 w2"
+							fill="currentColor"
+						/>
+						<span className="f6 ml3 pr2">Authorize Your Instagram</span>
+					</a>
+					<div className="tc mb2">
+						<p>
+							By authorizing, you agree to our{' '}
+							<a className="link " href="#">
+								Terms & Conditions
+							</a>
+						</p>
+					</div>
+				</div>
+				<div class="dtc-ns tc pv4" />
+			</div>
+		</div>
+	)
+}
+
+// const connector = connect(mapStateToProps, mapActionsToProps)
+
+export default Verify
