c1927acceeade8300761df7cf83e1d3b2cb8c289
Brandon Aiton <brandonaiton@icloud.com>
2017-11-12 13:04:14 -0500

    stylized the landing page

---
+++ src/components/_Home_Inputs.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+const {TextField} = require('t63');
+
+export default class Home_Inputs extends Component {
+  render() {
+    return (
+      <section className="input-container">
+        <div className="flex flex-column">
+        <h2 className="flex justify-center ts-subtitle">TURNING YOUR INSTAGRAM FEED INTO AN ONLINE SHOP.</h2>
+        <h2 className="flex justify-center mt0 instantly">INSTANTLY</h2>
+        </div>
+        <div className="custom-input">
+          <label>Instagram Username</label>
+          <TextField hintText="Username" floatingLabelText="Instagram Username"/><br/>
+          <label>Email Address</label>
+          <TextField hintText="Email" floatingLabelText="Email"/><br/>
+        </div>
+      </section>
+    )
+  }
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,66 +1,54 @@
 import history from '../history'
 import Product_Card from '../components/_Product_Card.js'
 import '../css/home.css'
+import TextField from 'material-ui/TextField';
+import Home_Inputs from '../components/_Home_Inputs.js'
+
 
 const React = require('react')
-const { Link } = require('react-router-dom')
-const { connect } = require('react-redux')
+const {Link} = require('react-router-dom')
+const {connect} = require('react-redux')
 
 class Home extends React.Component {
-	componentDidMount() {}
+  componentDidMount() {}
 
-	render() {
-		const props = this.props
-		const userName = this.props.location.pathname.substring(1)
-		return (
-			<div className="avenir">
-				<div className="flex justify-between">
-					<div>
-						<button
-							className="f6 link dim br-pill ph3 pv2 ma2 dib white bg-blue"
-							onClick={props.handleBack}
-						>
-							Back
-						</button>
-					</div>
-					<img
-						id="logo"
-						src="http://tagshop.co/assets/media/brand250.png"
-						alt="TagShop"
-					/>
-					<Link to="/cart">
-						<button className="f6 link dim br-pill ph3 pv2 ma2 dib white bg-blue">
-							Cart
-						</button>
-					</Link>
-				</div>
-			</div>
-		)
-	}
+  render() {
+    const props = this.props
+    const userName = this.props.location.pathname.substring(1)
+    const inputStyle = {
+      transformOrigin: "19.5px center"
+    }
+    return (
+      <div className="avenir">
+        <div className="flex justify-center">
+          <img id="logo" src="http://tagshop.co/assets/media/brand250.png" alt="TagShop"/>
+        </div>
+					<Home_Inputs/>
+      </div>
+    )
+  }
 }
 
 function mapActionsToProps(dispatch) {
-	const doDispatch = (type, field, value) => {
-		dispatch({
-			type: type + field,
-			payload: value
-		})
-	}
-	return {
-		dispatch,
-		handleUser: name => {
-			return e => {
-				doDispatch('SET_USER', null, name)
-			}
-		},
-		handleBack: e => history.goBack()
-	}
+  const doDispatch = (type, field, value) => {
+    dispatch({
+      type: type + field,
+      payload: value
+    })
+  }
+  return {
+    dispatch,
+    handleUser: name => {
+      return e => {
+        doDispatch('SET_USER', null, name)
+      }
+    },
+    handleBack: e => history.goBack()
+  }
 }
 
 const mapStateToProps = state => {
-	return {
-		user: state.user
-	}
+  return {user: state.user}
 }
 
 const connector = connect(mapStateToProps, mapActionsToProps)
