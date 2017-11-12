9c0b0c26d5c62f6651e1d5b398cbfd2864b7628d
Brandon Aiton <brandonaiton@icloud.com>
2017-11-12 13:05:35 -0500

    added an exclaimation mark lol

--- src/components/_Home_Inputs.js
+++ src/components/_Home_Inputs.js
@@ -7,7 +7,7 @@ export default class Home_Inputs extends Component {
       <section className="input-container">
         <div className="flex flex-column">
         <h2 className="flex justify-center ts-subtitle">TURNING YOUR INSTAGRAM FEED INTO AN ONLINE SHOP.</h2>
-        <h2 className="flex justify-center mt0 instantly">INSTANTLY</h2>
+        <h2 className="flex justify-center mt0 instantly">INSTANTLY!</h2>
         </div>
         <div className="custom-input">
           <label>Instagram Username</label>
