0186a5318cfb6fa6cc2a81fa24f2c0df20c0ef00
Pate Bryant <patebry@gmail.com>
2017-12-02 05:22:18 -0500

    tried to add carosel but could not find a good one

--- src/components/_Product_Card.js
+++ src/components/_Product_Card.js
@@ -7,7 +7,8 @@ import {
 	CardActions,
 	Card,
 	FlatButton,
-	RaisedButton
+	RaisedButton,
+	Chip
 } from 'material-ui'
 import '../css/product_card.css'
 import { Link } from 'react-router-dom'
@@ -15,29 +16,19 @@ const { contains, map, indexOf, length, remove } = require('ramda')
 const { SET_CART } = require('../constants')
 
 function Tags(tag) {
-	console.log('tag', tag)
 	return (
 		<div key={tag} style={{ paddingBottom: '8px' }}>
-			<FlatButton
-				backgroundColor="lightGrey"
-				style={{ borderRadius: '9999px' }}
-				label={tag}
-			/>
+			<Chip> {tag} </Chip>
 		</div>
 	)
 }
 
 function data(tags) {
 	const index = indexOf('tagshop', tags)
-	if (length(tags) === 1) {
-		return []
-	} else {
-		return remove(index, 1, tags)
-	}
+	return remove(index, 1, tags)
 }
 
 const Product_Card = props => {
-	console.log('tags', props.tags)
 	return (
 		<Card
 			style={{
@@ -47,60 +38,47 @@ const Product_Card = props => {
 			}}
 			key={props.media_id}
 		>
-			<CardHeader
-				titleStyle={{ verticalAlign: 'middle' }}
-				title={props.username}
-				avatar={props.profile_picture}
-			/>
-			<CardMedia>
-				<img
-					src={props.images[0]}
-					alt=""
-					style={{
-						height: '100%',
-						width: '100%',
-						margin: '0',
-						padding: '0'
-					}}
+			<Link to={'https://instagram.com/' + props.username} target="_blank">
+				<CardHeader
+					titleStyle={{ verticalAlign: 'middle' }}
+					title={props.username}
+					avatar={props.profile_picture}
 				/>
+			</Link>
+			<CardMedia>
+				<img src={props.images[0]} alt="" />
 			</CardMedia>
 			<CardTitle
 				style={{ paddingBottom: '0' }}
 				className="oswald flex"
 				title={'$' + props.price}
 			>
-				<CardActions style={{ flex: 'flex-end' }}>
-					<FlatButton
-						label="Details"
-						style={{ color: 'DeepPink', position: 'relative' }}
+				<FlatButton label="Details" style={{ color: 'DeepPink' }} />
+				{!contains(props.media_id, map(x => x.media_id, props.cart)) && (
+					<RaisedButton
+						backgroundColor="DeepPink"
+						labelStyle={{ color: 'white' }}
+						onClick={e => {
+							props.dispatch({
+								type: SET_CART,
+								payload: props
+							})
+						}}
+						style={{
+							float: 'right'
+						}}
+						label="Add to Cart"
 					/>
-
-					{!contains(props.media_id, map(x => x.media_id, props.cart)) && (
+				)}
+				{contains(props.media_id, map(x => x.media_id, props.cart)) && (
+					<Link to="/cart">
 						<RaisedButton
-							backgroundColor="DeepPink"
+							label="Cart"
 							labelStyle={{ color: 'white' }}
-							onClick={e => {
-								props.dispatch({
-									type: SET_CART,
-									payload: props
-								})
-							}}
-							style={{
-								float: 'right'
-							}}
-							label="Add to Cart"
+							backgroundColor="DeepPink"
 						/>
-					)}
-					{contains(props.media_id, map(x => x.media_id, props.cart)) && (
-						<Link to="/cart">
-							<RaisedButton
-								label="Cart"
-								labelStyle={{ color: 'white' }}
-								backgroundColor="DeepPink"
-							/>
-						</Link>
-					)}
-				</CardActions>
+					</Link>
+				)}
 			</CardTitle>
 			<CardText>{props.description}</CardText>
 			<CardActions style={{ width: '100%', flexWrap: 'wrap', display: 'flex' }}>
--- src/components/_Store_Header.js
+++ src/components/_Store_Header.js
@@ -21,24 +21,10 @@ class StoreHeader extends React.Component {
 				style={{
 					boxShadow: '0px 0px 5px black',
 					backgroundColor: 'DeepPink',
-					marginTop: '65px'
+					marginTop: '8px'
 				}}
 			>
 				<AppBar
-					title={
-						<img
-							id="logo"
-							src={props.user.user.media[0].profile_picture}
-							className="br-100 ba white ma2"
-							style={{
-								height: '50px',
-								width: '50px',
-								borderWidth: '2px',
-								backgroundColor: 'white'
-							}}
-							alt={tagshop}
-						/>
-					}
 					style={{
 						backgroundColor: 'DeepPink',
 						height: '65px',
@@ -48,6 +34,21 @@ class StoreHeader extends React.Component {
 					zDepth={0}
 					onLeftIconButtonTouchTap={this.handleToggle}
 				/>
+				<img
+					id="logo"
+					src={props.user.user.media[0].profile_picture}
+					className="br-100 ba white"
+					style={{
+						height: '50px',
+						width: '50px',
+						borderWidth: '2px',
+						backgroundColor: 'white',
+						position: 'relative',
+						zIndex: '2000',
+						top: '0'
+					}}
+					alt={tagshop}
+				/>
 				<Drawer
 					docked={false}
 					open={this.state.open}
@@ -55,10 +56,10 @@ class StoreHeader extends React.Component {
 				>
 					<MenuItem onClick={e => history.replace('/')} primaryText="Home" />
 				</Drawer>
-				<h2 className="tc white bg-DeepPink ma0 oswald">
+				<h2 className="tc white bg-DeepPink mt2 mb1 oswald">
 					{toUpper(props.user.user.media[0].username)}
 				</h2>
-				<div className="flex justify-between pr4 pl4 pb3 pt2 white f6">
+				<div className="flex justify-between pr2 pl2 pb2 white f6">
 					<FlatButton
 						label="RECENT"
 						style={{ color: 'white', borderRadius: '9999px' }}
